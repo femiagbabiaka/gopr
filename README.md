97f455081b7e556fd13b9da1ea0c39f572e95597
Femi Agbabiaka <femianthonyagbabiaka@gmail.com>
2016-09-24 18:36:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GoPR
+
+This is meant to be a pretty small module that does nothing except for creates pull requests using OAuth2, access tokens for 2fa, and Github's api.
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"os/exec"
+	"strings"
+)
+
+// PullRequest represents a the structure of our JSON body for Github.
+type PullRequest struct {
+	Title       string `json:"title"`
+	Target      string `json:"head"`
+	Base        string `json:"base"`
+	Description string `json:"body"`
+	requestURL  string
+}
+
+// TargetRepositoryInfo represents the repo section of our API endpoint for Github.
+type TargetRepositoryInfo struct {
+	Owner      string
+	Repository string
+}
+
+func main() {
+
+	var title, target, base, description, targetRepository string
+
+	flag.StringVar(&title, "title", "", "The title of your pull request.")
+	flag.StringVar(&target, "target", "", "The target branch of your pull request.")
+	flag.StringVar(&base, "base", currentBranch(), "The base branch for your pull request.")
+	flag.StringVar(&description, "description", "", "The description of your pull request.")
+	flag.StringVar(&targetRepository, "targetrepository", "", "The target repository for your pull request.")
+
+	flag.Parse()
+
+	if title == "" {
+		log.Fatal("Must supply a pull request title.")
+	}
+
+	if target == "" {
+		log.Fatal("Must supply a pull request target.")
+	}
+
+	if base == "" {
+		log.Fatal("Must supply a base branch for your pull request.")
+	}
+
+	if description == "" {
+		log.Fatal("Must supply a description for your pull request.")
+	}
+
+	if targetRepository == "" {
+		log.Fatal("Must supply a target repository for your pull request.")
+	}
+
+	requestRepositoryInfo := generateTargetRepositoryInfo(targetRepository)
+	requestURL := requestRepositoryInfo.createRequestURL()
+
+	pr := PullRequest{Title: title, Target: target, Base: base, Description: description, requestURL: requestURL}
+
+	res := pr.pullRequest()
+
+	os.Exit(0)
+}
+
+func generateTargetRepositoryInfo(tarRep string) TargetRepositoryInfo {
+	compositeStrings := strings.Split(tarRep, "/")
+
+	return TargetRepositoryInfo{Owner: compositeStrings[0], Repository: compositeStrings[1]}
+}
+
+// TODO: SUPPORT ENVIRONMENT VARIABLES FOR CONFIG STUFF
+func (r TargetRepositoryInfo) createRequestURL() string {
+	return fmt.Sprintf("/repos/%s/%s/pulls", r.Owner, r.Repository)
+}
+
+func (p PullRequest) pullRequest() bool {
+	client := &http.Client{Timeout: 0}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	requestBody := json.Marshal(p)
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("https://api.github.com%s", p.RequestURL), bytes.NewBuffer(requestBody))
+
+	//TODO: Set Headers here, read auth token and OAuth from Environment variables.
+}
+
+func currentBranch() string {
+	out, err := exec.Command("git branch | grep * | cut -d ' ' -f2").Output()
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+	return out
+}
